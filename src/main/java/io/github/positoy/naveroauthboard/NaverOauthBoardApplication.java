e1c49d7fe5d38474cc1c8791b1f1a397414bb125
positoy <positoy@gmail.com>
2020-08-07 00:05:51 +0900

    Initial commit usign start.spring.io

---
+++ src/main/java/io/github/positoy/naveroauthboard/NaverOauthBoardApplication.java
@@ -0,0 +1,13 @@
+package io.github.positoy.naveroauthboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverOauthBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaverOauthBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/positoy/naveroauthboard/NaverOauthBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.positoy.naveroauthboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaverOauthBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
